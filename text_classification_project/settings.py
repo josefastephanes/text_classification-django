d8ff001d2f069ac7f80deab13cca2b6cc5189460
josefastephanes <josefastephane8@gmail.com>
2026-07-07 21:56:11 +0530

    Updated few errors

--- text_classification_project/settings.py
+++ text_classification_project/settings.py
@@ -80,17 +80,6 @@ WSGI_APPLICATION = 'text_classification_project.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vehicle_parking_db',
-        'USER': 'root',
-        'PASSWORD': 'mysqldb(!@#$%^&*)',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
